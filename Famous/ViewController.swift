5e9d03f2db707550220a379570b9199f76629b95
Petr Shypila <ikrumping@gmail.com>
2017-02-07 21:58:45 +0100

    Initial Commit

---
+++ Famous/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Famous
+//
+//  Created by Petr Shypila on 07/02/2017.
+//  Copyright © 2017 Petr Shypila. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
